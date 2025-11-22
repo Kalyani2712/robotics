b2b00bfbfd674c6c899e307dd2873fb5f9cb0cbd
Kalyani Ramesh Khanavkar <162630430+Kalyani2712@users.noreply.github.com>
2025-11-22 21:51:48 +0530

    Add files via upload

---
+++ withgear.java
@@ -0,0 +1,25 @@
+import ch.aplu.robotsim.*;
+public class withgear
+{
+public withgear()
+{
+LegoRobot robot = new LegoRobot();
+Gear gear = new Gear();
+
+robot.addPart(gear);
+gear.setSpeed(20);
+gear.forward();
+Tools.delay(2000);      
+gear.left(2000);
+gear.forward(2000);
+gear.leftArc(0.2,2000);
+gear.forward(2000);
+gear.leftArc(-0.2,2000);
+gear.right(2000);
+
+robot.exit();
+  }
+  public static void main(String[] args) {
+     new withgear();
+  }
+  }
