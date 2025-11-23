2b31df80ebe3358d87fbe2d30f7c1e9cd3a83695
Kalyani Ramesh Khanavkar <162630430+Kalyani2712@users.noreply.github.com>
2025-11-23 13:29:28 +0530

    Add files via upload

---
+++ square.java
@@ -0,0 +1,22 @@
+import ch.aplu.robotsim.*;
+public class square
+{
+square()
+{
+TurtleRobot robot = new TurtleRobot();
+int i = 0;
+while (i < 4)
+{
+robot.forward(50);
+robot.right(90);
+i++;
+}
+robot.exit();
+ 
+} // end of while
+public static void main(String[] args) {
+       new square();
+}
+  
+
+  }
